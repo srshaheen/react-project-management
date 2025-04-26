adfe40518b06ed312562ba966e5e51f3e4a1dbd8
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-04-26 23:48:54 +0600

    very first commit of this project

---
+++ src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+import Button from "./Button";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 rounded-md shadow-md p-4 flex flex-col"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.body
+  );
+});
+
+export default Modal;
